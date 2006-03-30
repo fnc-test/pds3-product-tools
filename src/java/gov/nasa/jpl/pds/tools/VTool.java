839170952ae8cf393c1eecbda24dfea9f14aca5a
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-03-30 00:27:16 +0000

    Just setting up packages and a placeholder for VTool

---
+++ src/java/gov/nasa/jpl/pds/tools/VTool.java
@@ -0,0 +1,18 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ * Class to replace LVTool functionality
+ * 
+ */
+public class VTool {
+
+}
