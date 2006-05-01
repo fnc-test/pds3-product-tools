36e1bb3dd8aab37659e18966aa261b55b04d2412
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-05-01 07:25:57 +0000

    A TextString is a type of value associated with an attribute. See standards reference for full description.

---
+++ src/java/gov/nasa/jpl/pds/tools/label/TextString.java
@@ -0,0 +1,23 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.label;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class TextString extends Scalar {
+
+    /**
+     * @param value
+     */
+    public TextString(String value) {
+        super(value);
+    }
+
+}
