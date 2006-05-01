aa5dbe5b1a6080e9233a15d0cd9c748ae01c7d4e
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-05-01 07:22:20 +0000

    represents a comment in a label

---
+++ src/java/gov/nasa/jpl/pds/tools/label/CommentStatement.java
@@ -0,0 +1,42 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.label;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class CommentStatement extends Statement {
+    public final static String COMMENT_ID = "COMMENT-";
+    private String comment;
+
+    public CommentStatement(int lineNumber, String identifier, String comment) {
+        super(lineNumber, identifier);
+        this.comment = comment;
+    }
+    
+    /**
+     * @param lineNumber
+     * @param identifier
+     */
+    public CommentStatement(int lineNumber, String identifier) {
+        this(lineNumber, identifier, "");
+    }
+    
+    public CommentStatement(int lineNumber) {
+        this(lineNumber, COMMENT_ID + lineNumber, "");
+    }
+    
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+    public String getComment() {
+        return comment;
+    }
+}
