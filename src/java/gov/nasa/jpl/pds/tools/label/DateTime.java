8b85a4522ed595735cd180ffea2e3fc7958b6117
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-05-01 07:26:27 +0000

    A DateTime is a type of value associated with an attribute. See standards reference for full description.

---
+++ src/java/gov/nasa/jpl/pds/tools/label/DateTime.java
@@ -0,0 +1,22 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.label;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class DateTime extends Scalar {
+    /**
+     * @param value
+     */
+    public DateTime(String value) {
+        super(value);
+    }
+
+}
