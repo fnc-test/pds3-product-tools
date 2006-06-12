fc6554ad3995a36a06a0fb0035110c4d072e438f
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-06-12 13:15:37 +0000

    Error thrown if definition in dictionary is not correctly formed (i.e. missing part of the definition).

---
+++ src/java/gov/nasa/jpl/pds/tools/dict/parser/MalformedDefinitionException.java
@@ -0,0 +1,22 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.dict.parser;
+
+import gov.nasa.jpl.pds.tools.TraceableException;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class MalformedDefinitionException extends Exception {
+    private static final long serialVersionUID = 6035832581904662968L;
+
+    public MalformedDefinitionException(String message) {
+        super(message);
+    }
+}
