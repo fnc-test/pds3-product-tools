8f6639f6a96064343dc6334e3257874671b289c5
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-06-14 05:36:49 +0000

    Captures the names of valid types for element definitions in the data dictionary.

---
+++ src/java/gov/nasa/jpl/pds/tools/label/validate/Types.java
@@ -0,0 +1,33 @@
+// Copyright 2006, by the California Institute of 
+// Technology. ALL RIGHTS RESERVED. United States Government 
+// Sponsorship acknowledged. Any commercial use must be negotiated with 
+// the Office of Technology Transfer at the California Institute of 
+// Technology.
+//
+// This software may be subject to U.S. export control laws. By 
+// accepting this software, the user agrees to comply with all 
+// applicable U.S. export laws and regulations. User has the 
+// responsibility to obtain export licenses, or other export authority 
+// as may be required before exporting such information to foreign 
+// countries or providing access to foreign persons.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.label.validate;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public interface Types {
+    public final static String INTEGER = "INTEGER";
+    public final static String CHARACTER = "CHARACTER";
+    public final static String REAL = "REAL";
+    public final static String IDENTIFIER = "IDENTIFIER";
+    public final static String TIME = "TIME";
+    public final static String DATE = "DATE";
+    public final static String CONTEXT_DEPENDENT = "CONTEXTDEPENDENT";
+    public final static String NONDECIMAL = "NONDECIMAL";
+}
