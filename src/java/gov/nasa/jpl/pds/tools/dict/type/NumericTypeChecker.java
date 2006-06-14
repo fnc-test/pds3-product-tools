9e762288669fc75422ddf25f7047e30372e2f3da
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-06-14 19:44:28 +0000

    Add support to the interface for checking min and max values.

---
+++ src/java/gov/nasa/jpl/pds/tools/dict/type/NumericTypeChecker.java
@@ -0,0 +1,27 @@
+// Copyright 2006, by the California Institute of 
+// Technology. ALL RIGHTS RESERVED. United States Government 
+// Sponsorship acknowledged. Any commercial use must be negotiated with 
+// the Office of Technology Transfer at the California Institute of 
+// Technology.
+//
+// This software may be subject to U.S. export control laws. By 
+// accepting this software, the user agrees to comply with all 
+// applicable U.S. export laws and regulations. User has the 
+// responsibility to obtain export licenses, or other export authority 
+// as may be required before exporting such information to foreign 
+// countries or providing access to foreign persons.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.dict.type;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public interface NumericTypeChecker extends TypeChecker {
+    public void checkMinValue(String value, String min) throws OutOfRangeException, InvalidTypeException;
+    public void checkMaxValue(String value, String max) throws OutOfRangeException, InvalidTypeException;
+}
