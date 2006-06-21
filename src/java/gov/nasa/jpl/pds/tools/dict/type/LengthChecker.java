e41db317621e0b1382accf98602f053d89885b3c
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-06-21 15:59:20 +0000

    Simple class to check min and max lengths of values.

---
+++ src/java/gov/nasa/jpl/pds/tools/dict/type/LengthChecker.java
@@ -0,0 +1,41 @@
+// Copyright 2006, by the California Institute of 
+// Technology. ALL RIGHTS RESERVED. United States Government 
+// Sponsorship acknowledged. Any commercial use must be negotiated with 
+// the Office of Technology Transfer at the California Institute of 
+// Technology.
+//
+// This software may be subject to U.S. export control laws. By 
+// accepting this software, the user agrees to comply with all 
+// applicable U.S. export laws and regulations. User has the 
+// responsibility to obtain export licenses, or other export authority 
+// as may be required before exporting such information to foreign 
+// countries or providing access to foreign persons.
+//
+// $Id$ 
+//
+
+package gov.nasa.jpl.pds.tools.dict.type;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class LengthChecker {
+    
+    /* (non-Javadoc)
+     * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMinLength(java.lang.String, int)
+     */
+    public void checkMinLength(String value, int min) throws InvalidLengthException {
+        if (value.length() < min)
+            throw new InvalidLengthException(value + " is less than the acceptable minimum length of " + min);
+    }
+    
+    /* (non-Javadoc)
+     * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMaxLength(java.lang.String, int)
+     */
+    public void checkMaxLength(String value, int max) throws InvalidLengthException {
+        if (value.length() > max)
+            throw new InvalidLengthException(value + " exceed the maximum length of " + max);
+    }
+}
