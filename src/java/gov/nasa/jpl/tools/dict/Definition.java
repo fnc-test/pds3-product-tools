f753a58c6c09cc1a5245ed7697ae5be7a534c2c5
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-06-27 15:47:28 +0000

    update package structure per code review

---
+++ src/java/gov/nasa/jpl/tools/dict/Definition.java
@@ -0,0 +1,95 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.pds.tools.dict;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public abstract class Definition {
+    private String identifier;
+    private String statusType;
+    private String description;
+    private List aliases;
+    
+    public Definition(String identifier) {
+        this.identifier = identifier;
+        description = "";
+        statusType = "";
+        aliases = new ArrayList();
+    }
+
+    /**
+     * The aliases for this definition
+     * @return Returns the aliases.
+     */
+    public List getAliases() {
+        return aliases;
+    }
+    
+    /**
+     * Add an alias for this Definition
+     * @param alias The alias to add
+     */
+    public void addAlias(String alias) {
+        aliases.add(alias);
+    }
+
+    /**
+     * @param aliases The aliases to set.
+     */
+    public void setAliases(List aliases) {
+        this.aliases = aliases;
+    }
+
+    /**
+     * @return Returns the identifier.
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    /**
+     * @return Returns the description.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description The description to set.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return Returns the statusType.
+     */
+    public String getStatusType() {
+        return statusType;
+    }
+
+    /**
+     * @param statusType The statusType to set.
+     */
+    public void setStatusType(String statusType) {
+        this.statusType = statusType;
+    }
+
+    /**
+     * @param identifier The identifier to set.
+     */
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+}
