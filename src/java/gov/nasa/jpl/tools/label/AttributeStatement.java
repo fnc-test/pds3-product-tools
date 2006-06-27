b0df8dbc3e9b6a23fdef86334e15f3ae10b079d1
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-06-27 15:49:17 +0000

    update package structure per code review

---
+++ src/java/gov/nasa/jpl/tools/label/AttributeStatement.java
@@ -0,0 +1,108 @@
+//Copyright 2006, by the California Institute of 
+//Technology. ALL RIGHTS RESERVED. United States Government 
+//Sponsorship acknowledged. Any commercial use must be negotiated with 
+//the Office of Technology Transfer at the California Institute of 
+//Technology.
+//
+//This software may be subject to U.S. export control laws. By 
+//accepting this software, the user agrees to comply with all 
+//applicable U.S. export laws and regulations. User has the 
+//responsibility to obtain export licenses, or other export authority 
+//as may be required before exporting such information to foreign 
+//countries or providing access to foreign persons.
+//
+// $Id$ 
+//
+
+package gov.nasa.pds.tools.label;
+
+/**
+ * This class represents an attribute assignment in a PDS label file.
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class AttributeStatement extends Statement {
+    private Value value;
+    private String namespace;
+    private String elementIdentifier;
+    
+    /**
+     * Constructs a new attribute statement with no value
+     * @param lineNumber Line on which the statement starts
+     * @param identifier Uniquely identifies the statement
+     */
+    protected AttributeStatement(int lineNumber, String identifier) {
+        this(lineNumber, identifier, null);
+    }
+    
+    /**
+     * Constructs a new attribute statement with no line number or value
+     * @param identifier Uniquely identifies the statement
+     */
+    public AttributeStatement(String identifier) {
+        this(identifier, null);
+    }
+    
+    /**
+     * Constructs a new attribute statement with no line number
+     * @param identifier Uniquely identifies the statement
+     * @param value {@link Value} of the attribute
+     */
+    public AttributeStatement(String identifier, Value value) {
+        this(-1, identifier, value);
+    }
+    
+    /**
+     * 
+     * @param lineNumber Line on which the statement starts
+     * @param identifier Uniquely identifies the statement
+     * @param value {@link Value} of the attribute
+     */
+    public AttributeStatement(int lineNumber, String identifier, Value value) {
+        super(lineNumber, identifier);
+        
+        namespace = "";
+        if (identifier.indexOf(":") != -1)
+            namespace = identifier.substring(0, identifier.indexOf(":"));
+        
+        if (identifier.indexOf(":") == -1)
+            elementIdentifier = identifier;
+        else
+            elementIdentifier = identifier.substring(identifier.indexOf(":") + 1);
+        
+        this.value = value;
+    }
+    
+    /**
+     * Gets the namespace for this attribute
+     * @return The namespace or "" if none is found.
+     */
+    public String getNamespace() {
+        return namespace;
+    }
+    
+    /**
+     * Gets the unqualified identifier for the att
+     * @return Returns the element identifier.
+     */
+    public String getElementIdentifier() {
+        return elementIdentifier;
+    }
+
+    /**
+     * Retrieves the value of the attribute
+     * @return {@link Value} of the attribute
+     */
+    public Value getValue() {
+        return value;
+    }
+    
+    /**
+     * Sets the value for this attribute
+     * @param value {@link Value} of the attribute
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
+}
