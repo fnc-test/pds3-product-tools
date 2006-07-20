49abf5e0273807e5b3d27bbdf7f203c6049f9b21
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2006-07-20 15:28:05 +0000

    filled in some of the auto-generated method stubs in preparation for validation against a PDS dictionary

--- src/java/gov/nasa/pds/tools/dict/type/IdentifierChecker.java
+++ src/java/gov/nasa/pds/tools/dict/type/IdentifierChecker.java
@@ -16,37 +16,26 @@
 
 package gov.nasa.pds.tools.dict.type;
 
+import gov.nasa.pds.tools.label.validate.ElementValidator;
+import gov.nasa.pds.tools.dict.DictionaryTokens;
+
 /**
  * @author pramirez
  * @version $Revision$
  * 
  */
-public class IdentifierChecker implements TypeChecker {
+public class IdentifierChecker extends LengthChecker implements TypeChecker, DictionaryTokens {
 
     /* (non-Javadoc)
      * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#cast(java.lang.String)
      */
     public Object cast(String value) throws InvalidTypeException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /* (non-Javadoc)
-     * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMinLength(java.lang.String, int)
-     */
-    public void checkMinLength(String value, int min)
-            throws InvalidLengthException {
-        // TODO Auto-generated method stub
-
-    }
-
-    /* (non-Javadoc)
-     * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMaxLength(java.lang.String, int)
-     */
-    public void checkMaxLength(String value, int max)
-            throws InvalidLengthException {
-        // TODO Auto-generated method stub
-
+        if (!value.matches("[a-zA-Z][a-zA-Z0-9_]*"))
+            throw new InvalidTypeException(value + " is not a valid identifier.");
+        if (value.length() > ELEMENT_IDENT_LENGTH) 
+            throw new InvalidTypeException(value + " can not exceed " + ElementValidator.ELEMENT_IDENT_LENGTH + " characters.");
+        
+        return value;
     }
 
 }
