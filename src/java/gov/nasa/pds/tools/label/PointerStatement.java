5e1d61c14c741f4cf74d099ef5921560afb30f46
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-10 18:09:05 +0000

    Added some simple javadocs.

--- src/java/gov/nasa/pds/tools/label/PointerStatement.java
+++ src/java/gov/nasa/pds/tools/label/PointerStatement.java
@@ -7,6 +7,8 @@
 package gov.nasa.pds.tools.label;
 
 /**
+ * This class is the object representation of a pointer statement in a label.
+ * 
  * @author pramirez
  * @version $Revision$
  * 
@@ -16,29 +18,57 @@ public class PointerStatement extends Statement {
     private CommentStatement comment;
 
     /**
-     * @param lineNumber
-     * @param identifier
+     * Constructs essentially a null pointer
+     * @param lineNumber at which the statement occurs
+     * @param identifier of the statement
      */
     protected PointerStatement(int lineNumber, String identifier) {
         this(lineNumber, identifier, null);
     }
     
+    /**
+     * Constructs a pointer with a value on the right hand side
+     * @param lineNumber at which the statement occurs
+     * @param identifier of the statement
+     * @param value of the assignment
+     */
     public PointerStatement(int lineNumber, String identifier, Value value) {
         super(lineNumber, identifier);
         this.value = value; 
         comment = null;  
     }
     
+    /**
+     * Constructs a pointer with an unknown line number.
+     * @param identifier of the statement
+     * @param value of the assignment
+     */
     public PointerStatement(String identifier, Value value) {
         this(-1, identifier, value);
     }
     
+    /**
+     * Returns the value portion (right hand side) of the statement.
+     * @return value
+     */
     public Value getValue() { 
         return value;
     }
 
+    /**
+     * Attaches a comment to this pointer
+     * @param comment that occurs on same line as this pointer statement
+     */
     public void attachComment(CommentStatement comment) {
         this.comment = comment;
+    }
+    
+    /**
+     * Returns the comment that occurs on the same line as this pointer assigment
+     * @return comment
+     */
+    public CommentStatement getComment() {
+        return comment;
     }
 
 }
