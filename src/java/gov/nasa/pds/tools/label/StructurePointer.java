dadf1fd5aa7db904f623e1bd2a116894a3b5536f
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-10 18:07:22 +0000

    Update to extend an ExternalPointer. Essentially a refactoring to support checking of object references.

--- src/java/gov/nasa/pds/tools/label/StructurePointer.java
+++ src/java/gov/nasa/pds/tools/label/StructurePointer.java
@@ -7,10 +7,8 @@
 package gov.nasa.pds.tools.label;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import gov.nasa.pds.tools.label.parser.LabelParser;
@@ -18,29 +16,37 @@ import gov.nasa.pds.tools.label.parser.LabelParserFactory;
 import gov.nasa.pds.tools.label.parser.ParseException;
 
 /**
+ * This class represents a pointer that is a set of external statements that can and should
+ * be included in label containing this statement when performing validation.
  * @author pramirez
  * @version $Revision$
  * 
  */
-public class StructurePointer extends PointerStatement {
+public class StructurePointer extends ExternalPointer {
     public static String IDENTIFIER = "STRUCTURE";
     private List statements;
     private boolean loaded = false;
-    private URL labelURL;
 
-    public StructurePointer(int lineNumber, String identifier, Value value, URL base) throws MalformedURLException {
+    /**
+     * Constructs a pointer that can be resolved to a set of statements.
+     * @param lineNumber of statement
+     * @param identifier of statment
+     * @param value assigned to statment
+     */
+    public StructurePointer(int lineNumber, String identifier, Value value) {
         super(lineNumber, identifier, value);
         statements = new ArrayList();
-        labelURL = null;
-        if (base != null) {
-	        String url = base.toString();
-	        if (!url.endsWith("/"))
-	        	url += "/";
-	        labelURL = new URL(url + value.toString());
-        }
     }
     
-    public synchronized void loadReferencedStatements() throws ParseException, IOException {
+    /**
+     * This method attempts to load the referenced statements. If unsuccessful will throw an error.
+     * Once loaded the statements are held in the class so they may be accessed at a later time.
+     * @param includePaths An list of {@link URL} in which to search for the referenced file
+     * @throws ParseException thrown if the file can not be properly loaded
+     * @throws IOException thrown if file can not be accessed
+     */
+    public synchronized void loadReferencedStatements(List includePaths) throws ParseException, IOException {
+        URL labelURL = resolveURL(includePaths);
         if (!loaded) {
             loaded = true;
             LabelParser parser = LabelParserFactory.getInstance().newLabelParser();
@@ -49,8 +55,20 @@ public class StructurePointer extends PointerStatement {
         }
     }
     
+    /**
+     * Retrieves the list of statements pointed to by this structure pointer
+     * @return external list of statements
+     */
     public List getStatements() {
         return statements;
+    }
+    
+    /**
+     * Indicates whether or not the statments pointed to have been loaded.
+     * @return flag indicating load status
+     */
+    public boolean isLoaded() {
+        return loaded;
     }
 
 }
