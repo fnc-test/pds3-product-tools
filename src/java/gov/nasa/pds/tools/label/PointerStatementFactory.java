d1fce96a8e713d3031082bbb856d5569560f2197
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-10 18:08:01 +0000

    Support to generate ExternalPointers that aid in validation of object references.

--- src/java/gov/nasa/pds/tools/label/PointerStatementFactory.java
+++ src/java/gov/nasa/pds/tools/label/PointerStatementFactory.java
@@ -7,17 +7,27 @@
 package gov.nasa.pds.tools.label;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
+ * This class hides the construction of pointers. It helps determine the exact type of pointer and
+ * contstructs it. If modifications need to be made to this behavior it will be hidden here.
+ * 
  * @author pramirez
  * @version $Revision$
  * 
  */
 public class PointerStatementFactory {
-    public static PointerStatement newInstance(int line, String identifier, Value value, URL base) throws MalformedURLException {
+    public static PointerStatement newInstance(int line, String identifier, Value value) throws MalformedURLException {
+        //Essentially the pointer will be an internal or external pointer
+        //The two classes of external pointers we need to differentiate between are structure pointers which
+        //should allow us to load statments and just external references where we only care of the existence
+        //of the file which is pointed too
         if (identifier.endsWith(StructurePointer.IDENTIFIER))
-            return new StructurePointer(line, identifier, value, base);
+            return new StructurePointer(line, identifier, value);
+        else if (value instanceof TextString || value instanceof Sequence)
+            return new ExternalPointer(line, identifier, value);
+        
+        //We have just found a plain old pointer.
         return new PointerStatement(line, identifier, value);
     }
 }
