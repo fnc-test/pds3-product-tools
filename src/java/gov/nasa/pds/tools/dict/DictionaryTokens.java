5c3f14285e28a3b303f01b77fa34dc0e770c9efc
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-19 21:58:17 +0000

    Add flags to support standard value type in the data dictionary.

--- src/java/gov/nasa/pds/tools/dict/DictionaryTokens.java
+++ src/java/gov/nasa/pds/tools/dict/DictionaryTokens.java
@@ -49,6 +49,8 @@ public interface DictionaryTokens {
     public static final String ELEMENT_ALIASES = "ELEMENT_ALIAS_SEQUENCE";
     public static final String ALIAS_LIST = "ALIAS_LIST";
     public static final String UNIT_LIST = "UNIT_LIST";
+    public static final String VALUE_TYPE_SUGGESTED = "SUGGESTED";
+    public static final String VALUE_TYPE_STATIC = "STATIC";
     public static final int ELEMENT_IDENT_LENGTH = 30;
     public static final int NAMESPACE_LENGTH = 30;
 }
