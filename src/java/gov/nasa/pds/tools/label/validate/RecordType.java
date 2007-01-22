6b24ad034344fbaaec617d442216a58cb455bf75
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-22 22:10:03 +0000

    constants for record type element in a PDS label

---
+++ src/java/gov/nasa/pds/tools/label/validate/RecordType.java
@@ -0,0 +1,19 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.pds.tools.label.validate;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public interface RecordType {
+    public final String FIXED_LENGTH = "FIXED_LENGTH";
+    public final static String VARIABLE_LENGTH = "VARIABLE_LENGTH";
+    public final static String STREAM = "STREAM";
+    public final static String UNDEFINED = "UNDEFINED";
+}
