5f49c3049ec8504e8bf734bb2ae1b16f6e5f57fa
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-24 13:28:48 +0000

    Formats the log file for Vtool. The output is XML and adds a few fields that were not previously there.

---
+++ src/java/gov/nasa/pds/tools/logging/ToolsLogFormatter.java
@@ -0,0 +1,49 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.pds.tools.logging;
+
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class ToolsLogFormatter extends Formatter {
+
+    /* (non-Javadoc)
+     * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+     */
+    public String format(LogRecord record) {
+        StringBuffer buffer = new StringBuffer();
+        ToolsLogRecord toolsRecord = (ToolsLogRecord) record;
+        buffer.append(" <record>\n");
+        buffer.append("  <level>" + record.getLevel() + "</level>\n");
+        if (toolsRecord.getFile() != null)
+            buffer.append("  <file>" + toolsRecord.getFile() + "</file>\n");
+        if (toolsRecord.getLine() != -1)
+            buffer.append("  <line>" + toolsRecord.getLine() + "</line>\n");
+        if (toolsRecord.getRoot() != null)
+            buffer.append("  <rootFile>" + toolsRecord.getRoot() + "</rootFile>\n");
+        buffer.append("  <message>" + record.getMessage() + "</message>\n");
+        buffer.append(" </record>\n");
+        return buffer.toString();
+    }
+    
+    public String getHead(Handler handler) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<?xml version=\"1.0\" ?>\n");
+        buffer.append("<log>\n");
+        return buffer.toString();
+    }
+
+    public String getTail(Handler handler) {
+        return "</log>";
+    }
+}
