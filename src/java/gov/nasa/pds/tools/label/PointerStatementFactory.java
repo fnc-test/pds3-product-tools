50f9f5e74f4ff7b0a356ae2fd25d02f716eff364
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-01-26 01:15:56 +0000

    Fix the factory to appropriately determine the pointer type.

--- src/java/gov/nasa/pds/tools/label/PointerStatementFactory.java
+++ src/java/gov/nasa/pds/tools/label/PointerStatementFactory.java
@@ -16,18 +16,35 @@ import java.net.MalformedURLException;
  * @version $Revision$
  * 
  */
-public class PointerStatementFactory {
+public class PointerStatementFactory implements PointerType {
     public static PointerStatement newInstance(int line, String identifier, Value value) throws MalformedURLException {
         //Essentially the pointer will be an internal or external pointer
         //The two classes of external pointers we need to differentiate between are structure pointers which
         //should allow us to load statments and just external references where we only care of the existence
         //of the file which is pointed too
-        if (identifier.endsWith(StructurePointer.IDENTIFIER))
-            return new StructurePointer(line, identifier, value);
-        else if (value instanceof TextString || value instanceof Sequence)
-            return new ExternalPointer(line, identifier, value);
         
-        //We have just found a plain old pointer.
-        return new PointerStatement(line, identifier, value);
+        if (value instanceof TextString) {
+            for (int i = 0; i < INCLUDE_NAMES.length; i++) {
+                if (identifier.endsWith(INCLUDE_NAMES[i]))
+                    return new IncludePointer(line, identifier, value);
+            }
+            return new ExternalPointer(resolvePointerType(identifier), line, identifier, value);
+        } else if (value instanceof Sequence) {
+            return new ExternalPointer(resolvePointerType(identifier), line, identifier, value);
+        }
+        else
+            return new PointerStatement(resolvePointerType(identifier), line, identifier, value);
+    }
+    
+    private static int resolvePointerType(String identifier) {
+        int pointerType = UNDEFINED;
+        for (int i = 0; i < DESCRIPTION_NAMES.length && pointerType == UNDEFINED; i++) {
+            if (identifier.endsWith(DESCRIPTION_NAMES[i]))
+                pointerType = DESCRIPTION;
+        }
+        if (pointerType == UNDEFINED)
+            pointerType = DATA_LOCATION;
+        
+        return pointerType;
     }
 }
