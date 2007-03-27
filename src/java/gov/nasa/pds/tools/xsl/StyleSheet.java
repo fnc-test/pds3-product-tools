c49a81009be014ee1e3a59e87a5e3668cd8d5279
Michael Cayanan <Michael.D.Cayanan@jpl.nasa.gov>
2007-03-27 18:02:42 +0000

    The standard stylesheet names

---
+++ src/java/gov/nasa/pds/tools/xsl/StyleSheet.java
@@ -0,0 +1,12 @@
+package gov.nasa.pds.tools.xsl;
+
+/**
+ * The Standard Stylesheet names
+ * @author mcayanan
+ *
+ */
+public interface StyleSheet {
+	public final String FULLXSL = "report-full.xsl";
+	public final String MINXSL = "report-min.xsl";
+	public final String SUMXSL = "report-sum.xsl";
+}
