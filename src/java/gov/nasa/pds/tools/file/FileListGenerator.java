f719be7b4a8ed446f4743f230415525aadd49c50
Michael Cayanan <Michael.D.Cayanan@jpl.nasa.gov>
2007-04-04 17:51:34 +0000

    Add license at top of source

--- src/java/gov/nasa/pds/tools/file/FileListGenerator.java
+++ src/java/gov/nasa/pds/tools/file/FileListGenerator.java
@@ -1,8 +1,17 @@
-//	Copyright (c) 2005, California Institute of Technology.
-//	ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
-//
-//	 $Id: FileListGenerator.java 1429 2006-07-14 22:25:54Z mcayanan $ 
-//
+//Copyright 2006-2007, by the California Institute of 
+//Technology. ALL RIGHTS RESERVED. United States Government 
+//Sponsorship acknowledged. Any commercial use must be negotiated with 
+//the Office of Technology Transfer at the California Institute of 
+//Technology.
+
+//This software may be subject to U.S. export control laws. By 
+//accepting this software, the user agrees to comply with all 
+//applicable U.S. export laws and regulations. User has the 
+//responsibility to obtain export licenses, or other export authority 
+//as may be required before exporting such information to foreign 
+//countries or providing access to foreign persons.
+
+// $Id: FileListGenerator.java 1429 2006-07-14 22:25:54Z mcayanan $ 
 
 package gov.nasa.pds.tools.file;
 
