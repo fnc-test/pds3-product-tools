a01fc4f6f98812c08c42f3bd329564fe9067aa82
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-04-06 17:23:14 +0000

    Update copyright. Update rules for making a value match. Changed warning message for manipulation of value.

--- src/java/gov/nasa/pds/tools/label/validate/ElementValidator.java
+++ src/java/gov/nasa/pds/tools/label/validate/ElementValidator.java
@@ -1,15 +1,14 @@
-// Copyright 2006, by the California Institute of 
-// Technology. ALL RIGHTS RESERVED. United States Government 
-// Sponsorship acknowledged. Any commercial use must be negotiated with 
-// the Office of Technology Transfer at the California Institute of 
-// Technology.
+// Copyright 2006-2007, by the California Institute of Technology.
+// ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
+// Any commercial use must be negotiated with the Office of Technology Transfer
+// at the California Institute of Technology.
 //
-// This software may be subject to U.S. export control laws. By 
-// accepting this software, the user agrees to comply with all 
-// applicable U.S. export laws and regulations. User has the 
-// responsibility to obtain export licenses, or other export authority 
-// as may be required before exporting such information to foreign 
-// countries or providing access to foreign persons.
+// This software is subject to U. S. export control laws and regulations
+// (22 C.F.R. 120-130 and 15 C.F.R. 730-774). To the extent that the software
+// is subject to U.S. export control laws and regulations, the recipient has
+// the responsibility to obtain export licenses or other export authority as
+// may be required before exporting such information to foreign countries or
+// providing access to foreign nationals.
 //
 // $Id$ 
 //
@@ -109,9 +108,13 @@ public class ElementValidator implements DictionaryTokens {
                         
                         if (definition.getValues().contains(filteredValue)) {
                             foundValue = true;
+                        } else if (definition.getValues().contains(filteredValue.toUpperCase())) {
+                            //Matches if value match can be made by simply switching case.
+                            manipulated = true;
+                            foundValue = true;
                         } else {
                             //Continue with whitespace striping
-                            filteredValue = Utility.filterString(filteredValue);
+                            filteredValue = Utility.filterString(filteredValue.toUpperCase());
                             manipulated = true;
                             if (definition.getValues().contains(filteredValue))
                                 foundValue = true;
@@ -131,8 +134,8 @@ public class ElementValidator implements DictionaryTokens {
                             }
                         } else if (manipulated) {
                             //Value had to be manipulated to make a match
-                            log.log(new ToolsLogRecord(Level.INFO, "Manipulated value for " + attribute.getIdentifier() +
-                                       " to find valid value.", attribute.getFilename(), attribute.getContext(), attribute.getLineNumber()));
+                            log.log(new ToolsLogRecord(Level.INFO, "Element value was manipulated for " + attribute.getIdentifier() +
+                                       " in order to match value list.", attribute.getFilename(), attribute.getContext(), attribute.getLineNumber()));
                         }
                     }
                 }
