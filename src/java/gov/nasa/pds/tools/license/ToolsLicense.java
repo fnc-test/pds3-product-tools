4124069510c4495efdcb7bba8093905567f197a6
Michael Cayanan <Michael.D.Cayanan@jpl.nasa.gov>
2007-07-31 20:49:44 +0000

    Software license.

---
+++ src/java/gov/nasa/pds/tools/license/ToolsLicense.java
@@ -0,0 +1,21 @@
+package gov.nasa.pds.tools.license;
+
+/**
+ * Contains the software license.
+ * 
+ * @author mcayanan
+ *
+ */
+public interface ToolsLicense {
+	
+	public final String LICENSE = "\nCopyright 2006-2007, by the California Institute of Technology. ALL\n"
+								+ "RIGHTS RESERVED. United States Government Sponsorship acknowledged.\n"
+								+ "Any commercial use must be negotiated with the Office of Technology\n"
+								+ "Transfer at the California Institute of Technology. This software\n"
+								+ "may be subject to U.S. export control laws. By accepting this\n"
+								+ "software, the user agrees to comply with all applicable U.S. export\n"
+								+ "laws and regulations. User has the responsibility to obtain export\n"
+								+ "licenses, or other export authority as may be required before\n"
+								+ "exporting such information to foreign countries or providing access\n"
+								+ "to foreign persons.";
+}
