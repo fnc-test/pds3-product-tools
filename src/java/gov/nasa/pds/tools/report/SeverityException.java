7e53f180b42b8efcfe7368aa885ca12ee3318bfe
Michael Cayanan <Michael.D.Cayanan@jpl.nasa.gov>
2007-09-25 15:03:19 +0000

    Removed exception class (not needed)

--- src/java/gov/nasa/pds/tools/report/SeverityException.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.pds.tools.report;
-
-public class SeverityException extends Exception {
-	private static final long serialVersionUID = -3658793797408109943L;
-	
-	public SeverityException(String msg) {
-		super(msg);
-	}
-}
