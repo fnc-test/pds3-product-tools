ac0bc5f98e3faaac33f9cffe6d4744807686fdcb
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2007-09-27 13:10:05 +0000

    A default implementation of a validation listener that does nothing when errors and warnings are reported. Use to simplify code in places where only a filler is needed.

---
+++ src/java/gov/nasa/pds/tools/label/validate/DefaultValidationListener.java
@@ -0,0 +1,32 @@
+//Copyright (c) 2005, California Institute of Technology.
+//ALL RIGHTS RESERVED. U.S. Government sponsorship acknowledged.
+//
+// $Id$ 
+//
+
+package gov.nasa.pds.tools.label.validate;
+
+/**
+ * @author pramirez
+ * @version $Revision$
+ * 
+ */
+public class DefaultValidationListener implements ValidationListener {
+
+    /* (non-Javadoc)
+     * @see gov.nasa.pds.tools.label.validate.ValidationListener#reportError(java.lang.String)
+     */
+    public void reportError(String error) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see gov.nasa.pds.tools.label.validate.ValidationListener#reportWarning(java.lang.String)
+     */
+    public void reportWarning(String warning) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
