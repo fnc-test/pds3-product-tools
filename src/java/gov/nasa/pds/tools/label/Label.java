330544e7dc7e0ec00ca8fa52462d8c4312378021
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2008-07-14 16:50:00 +0000

    Allow status to be set to SKIP

--- src/java/gov/nasa/pds/tools/label/Label.java
+++ src/java/gov/nasa/pds/tools/label/Label.java
@@ -267,8 +267,10 @@ public class Label implements LabelType, Status {
            //Drop everything else
            if (PASS.equals(status) && UNKNOWN.equals(this.status))
               this.status = PASS;
-           else if (FAIL.equals(status))
+           else if (FAIL.equals(status) && UNKNOWN.equals(this.status))
               this.status = FAIL;
+           else if (SKIP.equals(status))
+        	  this.status = SKIP;
         }
     }
     
