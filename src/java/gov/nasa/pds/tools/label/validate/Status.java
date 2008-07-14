89afa33db6bca23996f2c227807aeec3b40be45e
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2008-07-14 15:39:09 +0000

    Add skip flag

--- src/java/gov/nasa/pds/tools/label/validate/Status.java
+++ src/java/gov/nasa/pds/tools/label/validate/Status.java
@@ -23,5 +23,6 @@ package gov.nasa.pds.tools.label.validate;
 public interface Status {
     public static final String PASS = "PASS";
     public static final String FAIL = "FAIL";
+    public static final String SKIP = "SKIP";
     public static final String UNKNOWN = "UNKNOWN";
 }
