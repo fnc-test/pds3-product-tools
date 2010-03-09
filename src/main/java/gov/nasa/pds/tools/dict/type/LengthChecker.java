2fb37e1ccce150246abfd84a075bb79a90f68203
Michael Cayanan <Michael.D.Cayanan@jpl.nasa.gov>
2010-03-09 17:49:18 +0000

    Resolve PDSEN-307. Truncate a long value if it exceeds the acceptable maximum length of the element definition.

--- src/main/java/gov/nasa/pds/tools/dict/type/LengthChecker.java
+++ src/main/java/gov/nasa/pds/tools/dict/type/LengthChecker.java
@@ -31,9 +31,8 @@ public class LengthChecker {
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMinLength(java
-     * .lang.String, int)
+     * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMinLength(java
+     *      .lang.String, int)
      */
     public void checkMinLength(String value, ElementDefinition def,
             AttributeStatement attribute) throws InvalidLengthException {
@@ -50,20 +49,46 @@ public class LengthChecker {
     /*
      * (non-Javadoc)
      * 
-     * @see
-     * gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMaxLength(java
-     * .lang.String, int)
+     * @see gov.nasa.jpl.pds.tools.label.validate.TypeChecker#checkMaxLength(java
+     *      .lang.String, int)
      */
     public void checkMaxLength(String value, ElementDefinition def,
             AttributeStatement attribute) throws InvalidLengthException {
         final int max = def.getMaxLength();
         final String compressedVal = StrUtils.stripPadding(value);
+        int cutOffLength = 65;
         if (compressedVal.length() > max) {
-            throw new InvalidLengthException(
-                    attribute,
+            String truncatedVal = null;
+            // Perform truncation so that a really long value does not have to
+            // be thrown in its entirety into the error message.
+            if (compressedVal.length() > cutOffLength) {
+                boolean done = false;
+                // If the truncation lands in the middle of the word, capture
+                // the rest of the word
+                while (!done) {
+                    try {
+                        if (Character.isLetterOrDigit(compressedVal
+                                .charAt(cutOffLength)))
+                            ++cutOffLength;
+                        else {
+                            done = true;
+                            truncatedVal = compressedVal.substring(0,
+                                    cutOffLength)
+                                    + "..."; //$NON-NLS-1$
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        // We've reached the end of the string value
+                        done = true;
+                        truncatedVal = compressedVal;
+                    }
+                }
+            } else {
+                truncatedVal = compressedVal;
+            }
+            throw new InvalidLengthException(attribute,
                     "parser.error.tooLong", ProblemType.EXCESSIVE_VALUE_LENGTH, //$NON-NLS-1$
-                    value, compressedVal.length(), max, def.getIdentifier(),
-                    def.getDataType());
+                    truncatedVal, compressedVal.length(), max, def
+                            .getIdentifier(), def.getDataType());
         }
     }
 }
