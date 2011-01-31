89c2b29c39c36cdc9e5bdabc853c213e460b3bf1
Paul Ramirez <Paul.M.Ramirez@jpl.nasa.gov>
2011-01-31 22:20:37 +0000

    Added utility method that should have been there for getting the source of a statement as a string. This mimics the method on a Label and can be used for comparison.

--- src/main/java/gov/nasa/pds/tools/label/Statement.java
+++ src/main/java/gov/nasa/pds/tools/label/Statement.java
@@ -38,127 +38,134 @@ import org.antlr.runtime.Token;
  */
 public abstract class Statement implements Comparable<Object> {
 
-    protected final int lineNumber;
+  protected final int lineNumber;
 
-    protected final DictIdentifier identifier;
+  protected final DictIdentifier identifier;
 
-    protected final URI sourceURI;
+  protected final URI sourceURI;
 
-    protected final File sourceFile;
+  protected final File sourceFile;
 
-    protected final Label label;
+  protected final Label label;
 
-    protected CommentStatement comment;
+  protected CommentStatement comment;
 
-    public Statement(final Label sourceLabel, final int lineNumber,
-            final DictIdentifier identifier) {
-        this.label = sourceLabel;
-        this.lineNumber = lineNumber;
-        this.identifier = identifier;
-        if (sourceLabel == null) {
-            this.sourceURI = null;
-            this.sourceFile = null;
-        } else {
-            final URI uri = sourceLabel.getLabelURI();
-            if (uri != null) {
-                this.sourceURI = uri;
-                this.sourceFile = null;
-            } else {
-                this.sourceURI = null;
-                this.sourceFile = sourceLabel.getLabelFile();
-            }
-        }
+  public Statement(final Label sourceLabel, final int lineNumber,
+      final DictIdentifier identifier) {
+    this.label = sourceLabel;
+    this.lineNumber = lineNumber;
+    this.identifier = identifier;
+    if (sourceLabel == null) {
+      this.sourceURI = null;
+      this.sourceFile = null;
+    } else {
+      final URI uri = sourceLabel.getLabelURI();
+      if (uri != null) {
+        this.sourceURI = uri;
+        this.sourceFile = null;
+      } else {
+        this.sourceURI = null;
+        this.sourceFile = sourceLabel.getLabelFile();
+      }
     }
+  }
 
-    /**
-     * Retrieves the label associated with this statement
-     * 
-     * @return label
-     */
-    public Label getLabel() {
-        return this.label;
-    }
+  /**
+   * Retrieves the label associated with this statement
+   * 
+   * @return label
+   */
+  public Label getLabel() {
+    return this.label;
+  }
 
-    /**
-     * Retrieves the line for this statement
-     * 
-     * @return The line on which the statement starts
-     */
-    public int getLineNumber() {
-        return this.lineNumber;
-    }
+  /**
+   * Retrieves the line for this statement
+   * 
+   * @return The line on which the statement starts
+   */
+  public int getLineNumber() {
+    return this.lineNumber;
+  }
 
-    /**
-     * Retrieves the identifier for the statement
-     * 
-     * @return unique identifier
-     */
-    public DictIdentifier getIdentifier() {
-        return this.identifier;
-    }
+  /**
+   * Retrieves the identifier for the statement
+   * 
+   * @return unique identifier
+   */
+  public DictIdentifier getIdentifier() {
+    return this.identifier;
+  }
 
-    public URI getSourceURI() {
-        return this.sourceURI;
-    }
+  public URI getSourceURI() {
+    return this.sourceURI;
+  }
 
-    public File getSourceFile() {
-        return this.sourceFile;
-    }
+  public File getSourceFile() {
+    return this.sourceFile;
+  }
 
-    // TODO: determine if multiple comments allowed for a single statement
-    public void attachComment(final Token commentToken) {
-        if (commentToken != null) {
-            this.comment = new CommentStatement(this.label, commentToken,
-                    this.identifier.getId());
-        }
+  public String getSourceString() {
+    if (this.sourceFile != null) {
+      return this.sourceFile.toString();
     }
+    return this.sourceURI.toString();
+  }
 
-    /**
-     * Returns the comment associated with statement
-     * 
-     * @return comment
-     */
-    public CommentStatement getComment() {
-        return this.comment;
+  // TODO: determine if multiple comments allowed for a single statement
+  public void attachComment(final Token commentToken) {
+    if (commentToken != null) {
+      this.comment = new CommentStatement(this.label, commentToken,
+          this.identifier.getId());
     }
+  }
 
-    public int compareTo(Object o) {
-        return this.getLineNumber() - ((Statement) o).getLineNumber();
-    }
+  /**
+   * Returns the comment associated with statement
+   * 
+   * @return comment
+   */
+  public CommentStatement getComment() {
+    return this.comment;
+  }
 
-    protected void validateType(final DictIdentifier id,
-            final Class<? extends Definition> clazz) {
-        if (!id.getType().equals(clazz)) {
-            throw new RuntimeException("ID type mismatch! Expected \"" //$NON-NLS-1$
-                    + clazz.toString() + "\" but got \"" //$NON-NLS-1$
-                    + id.getType().toString() + "\"."); //$NON-NLS-1$
-        }
-    }
+  public int compareTo(Object o) {
+    return this.getLineNumber() - ((Statement) o).getLineNumber();
+  }
 
-    protected void validateObjectType(final DictIdentifier id) {
-        validateType(id, ObjectDefinition.class);
+  protected void validateType(final DictIdentifier id,
+      final Class<? extends Definition> clazz) {
+    if (!id.getType().equals(clazz)) {
+      throw new RuntimeException("ID type mismatch! Expected \"" //$NON-NLS-1$
+          + clazz.toString() + "\" but got \"" //$NON-NLS-1$
+          + id.getType().toString() + "\"."); //$NON-NLS-1$
     }
+  }
 
-    protected void validateGroupType(final DictIdentifier id) {
-        validateType(id, GroupDefinition.class);
-    }
+  protected void validateObjectType(final DictIdentifier id) {
+    validateType(id, ObjectDefinition.class);
+  }
 
-    protected void validateElementType(final DictIdentifier id) {
-        validateType(id, ElementDefinition.class);
-    }
+  protected void validateGroupType(final DictIdentifier id) {
+    validateType(id, GroupDefinition.class);
+  }
 
-    protected void validateCommentType(final DictIdentifier id) {
-        validateType(id, NullDefinition.class);
-    }
+  protected void validateElementType(final DictIdentifier id) {
+    validateType(id, ElementDefinition.class);
+  }
 
-    protected void validatePointerType(final DictIdentifier id) {
-        validateType(id, NullDefinition.class);
-    }
+  protected void validateCommentType(final DictIdentifier id) {
+    validateType(id, NullDefinition.class);
+  }
 
-    @SuppressWarnings("nls")
-    @Override
-    public String toString() {
-        return this.identifier.toString() + " "
-                + StrUtils.getNonNull(this.sourceFile, this.sourceURI);
-    }
+  protected void validatePointerType(final DictIdentifier id) {
+    validateType(id, NullDefinition.class);
+  }
+
+  @SuppressWarnings("nls")
+  @Override
+  public String toString() {
+    return this.identifier.toString() + " "
+        + StrUtils.getNonNull(this.sourceFile, this.sourceURI);
+  }
 }
